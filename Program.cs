cbaaa74ea0d1a31faf9ce72ebfa5bdf65e65a660
RenoNovoa <djnova25@gmail.com>
2021-01-12 17:50:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
