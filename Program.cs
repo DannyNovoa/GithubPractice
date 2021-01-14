8a696e0b1ebee6f5f3310ec6b716ade8d46152a9
RenoNovoa <djnova25@gmail.com>
2021-01-14 11:34:15 -0500

    Exercise6.7

--- Program.cs
+++ Program.cs
@@ -8,9 +8,49 @@ namespace GithubPractice
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("We are writing all the codes!!");
+            int num1 = 5;
+            int num2 = num1;
 
-            Console.WriteLine("We added more codes!");
+
+            num1 = 6;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            string answer;
+            do
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine();
+                Console.WriteLine("Would you like to continue (y/n) ?");
+                 answer = Console.ReadLine();
+                if (answer == "n")
+                {
+                    Console.WriteLine("Adios Amigo!");
+                }
+                Console.WriteLine();
+
+
+            } while (answer == "y");
+            string newAnswer;
+            do
+            {
+                Console.WriteLine("Hi dude enter some text please: ");
+                newAnswer = Console.ReadLine();
+                Console.WriteLine($"Hello world {newAnswer} !");
+
+                Console.WriteLine("Would you like to continue (y/n) ?");
+                newAnswer = Console.ReadLine();
+                if (newAnswer == "n")
+                {
+                    Console.WriteLine("Adios, A bientot! ");
+                }
+
+
+            } while (newAnswer == "y");
+            
+
+
+            Console.ReadKey();
+          
         }
     }
 }
